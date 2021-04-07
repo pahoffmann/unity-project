a2a4abec6ac60f9bf555dfcd967a08f193f58220
Patrick <pahoffmann@uni-osnabrueck.de>
2021-04-07 22:44:32 +0200

    removed unused dependencies

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using UnityEditor.Build.Reporting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
