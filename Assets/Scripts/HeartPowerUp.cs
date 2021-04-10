036cf65c09f134431ce779e236f4b426ab002a59
Carina König <clkoenig96@web.de>
2021-04-10 13:46:44 +0200

    1UP Heart Power Up

---
+++ Assets/Scripts/HeartPowerUp.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartPowerUp : MonoBehaviour
+{
+    private float _heartPowerUpSpeed = 3f;
+
+
+    public AudioClip nicesound;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, -_heartPowerUpSpeed * Time.deltaTime, 0f, Space.World);
+        if (transform.position.y < -6f)
+        {
+            Debug.Log("Bottom boundary has been called");
+            Destroy(this.gameObject);
+        }
+    }
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // collision handling
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("nicesound should be played");
+            AudioSource.PlayClipAtPoint(nicesound, transform.position);
+            Debug.Log("heart hit player");
+            
+            //hier muss noch die Funktion hin, die das Leben um 1 erhöht
+            
+            
+            
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+}
