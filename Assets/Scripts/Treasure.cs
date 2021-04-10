6e57931066d6b526da7b9256483b984d71487f83
Carina König <clkoenig96@web.de>
2021-04-10 13:13:10 +0200

    new PowerUp Treasure

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    private float _treasurePowerUpSpeed = 5f;
+
+
+    public AudioClip moneysound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bling = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, -_treasurePowerUpSpeed * Time.deltaTime, 0f, Space.World);
+        
+        BottomBoundary();
+    }
+
+    private void BottomBoundary()
+    {
+        Debug.Log("Bottom boundary has been called");
+        if (transform.position.y < -6f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // collision handling
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("moneysound should be played");
+            AudioSource.PlayClipAtPoint(moneysound, transform.position);
+            Debug.Log("treasure hit player");
+            
+            //hier muss noch die Funktion hin, die dem Score mehr Punkte gibt (das Power up gibt einem Punkte)
+            
+            
+            
+            Destroy(this.gameObject);
+        }
+        
+    }
+   
+
+}
